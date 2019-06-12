b5a7d30261adf5beddf8f4ba58978f15e853aa0f
Keanu Allridge <kallridge92@gmail.com>
2019-06-12 06:00:55 -0700

    Add properties

--- ContactsApi/Contacts/Contact.cs
+++ ContactsApi/Contacts/Contact.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using ContactsApi.Addresses;
+using ContactsApi.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ContactsApi.Users;
 
 namespace ContactsApi.Contacts
 {
     public class Contact
     {
         public int Id { get; set; }
-        public User User { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string HomePhone { get; set; }
+        public string CellPhone { get; set; }
+
+        // Navigation Properties
+        public int UserId { get; set; }
+        public ICollection<Address> Addresses { get; set; }
     }
 }
