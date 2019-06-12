b3fc9bca93be7e8191a79c41cfbcddd26e8fa577
Keanu Allridge <kallridge92@gmail.com>
2019-06-12 15:41:12 -0700

    Add constructor

--- ContactsApi/Users/UserDto.cs
+++ ContactsApi/Users/UserDto.cs
@@ -7,21 +7,29 @@ using System.Threading.Tasks;
 
 namespace ContactsApi.Users
 {
-    public class UserDto
+  public class UserDto
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public IEnumerable<Contact> Contacts { get; set; }
+
+    public UserDto(User user)
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public IEnumerable<Contact> Contacts { get; set; }
-
-        public UserDto(User user)
-        {
-            Id = user.Id;
-            FirstName = user.FirstName;
-            LastName = user.LastName;
-            Contacts = user.Contacts;
-        }
-
-        public UserDto() { }
+      Id = user.Id;
+      FirstName = user.FirstName;
+      LastName = user.LastName;
+      Contacts = user.Contacts;
     }
+
+    public UserDto(int id, string firstName, string lastName, IEnumerable<Contact> contacts)
+    {
+      Id = id;
+      FirstName = firstName;
+      LastName = lastName;
+      Contacts = contacts;
+    }
+
+    public UserDto() { }
+  }
 }
