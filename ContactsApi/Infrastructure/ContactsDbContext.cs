acf603d51fcffa01ac94e69b6e7ef6f2e85b0113
Keanu Allridge <kallridge92@gmail.com>
2019-06-12 15:40:51 -0700

    Add DbContextOpts to constructor

--- ContactsApi/Infrastructure/ContactsDbContext.cs
+++ ContactsApi/Infrastructure/ContactsDbContext.cs
@@ -9,38 +9,39 @@ using System.Threading.Tasks;
 
 namespace ContactsApi.Infrastructure
 {
-    public class ContactsDbContext : DbContext
+  public class ContactsDbContext : DbContext
+  {
+    DbSet<Address> Addresses { get; set; }
+    DbSet<Contact> Contacts { get; set; }
+    DbSet<User> Users { get; set; }
+
+    public ContactsDbContext(DbContextOptions<ContactsDbContext> options) : base(options)
+    { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
-        public ContactsDbContext() { }
-
-        DbSet<Address> Addresses { get; set; }
-        DbSet<Contact> Contacts { get; set; }
-        DbSet<User> Users { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder builder)
-        {
-            //base.OnConfiguring(optionsBuilder);
-            builder.UseSqlite("Data Source=ContactsApi.db;");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Configure User
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasMany<Contact>(user => user.Contacts)
-                .WithOne(contact => contact.User);
-            });
-
-            // Configure Contacts
-            modelBuilder.Entity<Contact>(entity =>
-            {
-                entity
-                .HasMany<Address>(contact => contact.Addresses)
-                .WithOne(address => address.Contact);
-            });
-        }
+      builder.UseSqlite("Data Source=ContactsApi.db;");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+
+      // Configure User
+      modelBuilder.Entity<User>(entity =>
+      {
+        entity.HasMany<Contact>(user => user.Contacts)
+              .WithOne(contact => contact.User);
+      });
+
+      // Configure Contacts
+      modelBuilder.Entity<Contact>(entity =>
+      {
+        entity
+              .HasMany<Address>(contact => contact.Addresses)
+              .WithOne(address => address.Contact);
+      });
+    }
+  }
 }
