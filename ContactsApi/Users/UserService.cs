081fadb755c6fbb6497788d4423c1351fd779422
Keanu Allridge <kallridge92@gmail.com>
2019-06-13 06:03:53 -0700

    Rename method

--- ContactsApi/Users/UserService.cs
+++ ContactsApi/Users/UserService.cs
@@ -1,11 +1,10 @@
 namespace ContactsApi.Users
 {
-  public class UserService : IUserService
-  {
-    // TODO: Add UserFactory and UserDtoFactory
-    public UserDto ConvertToDto(User user)
+    public class UserService : IUserService
     {
-      return new UserDto(user);
+        public UserDto ConvertToDtoFromUser(User user)
+        {
+            return new UserDto(user);
+        }
     }
-  }
 }
