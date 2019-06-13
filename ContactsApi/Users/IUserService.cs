1e7a1e21b08c5a09c36d6ead2ad796d66796ef03
Keanu Allridge <kallridge92@gmail.com>
2019-06-12 19:28:53 -0700

    Add comment

--- ContactsApi/Users/IUserService.cs
+++ ContactsApi/Users/IUserService.cs
@@ -1,7 +1,13 @@
 namespace ContactsApi.Users
 {
-  public interface IUserService
-  {
-    UserDto ConvertToDto(User user);
-  }
+    public interface IUserService
+    {
+        /// <summary>
+        /// Converts a given User object into a UserDto by instantiating an object
+        /// without any ASP.NET Identity fields and properties.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>UserDto</returns>
+        UserDto ConvertToDto(User user);
+    }
 }
