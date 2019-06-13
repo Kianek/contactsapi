9342675614bef050006ed80abc73a42cc54a16a2
Keanu Allridge <kallridge92@gmail.com>
2019-06-13 07:03:39 -0700

    Initial commit

---
+++ ContactsApi.Tests/Users/UserServiceTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using ContactsApi.Users;
+using FluentAssertions;
+
+namespace ContactsApi.Tests
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ContactsApi/Users/UserRegistrationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApi.Users
+{
+    public class UserRegistrationDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public UserRegistrationDto(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
