a1e7db6e2fdae7913c08fde9e480a9838c61bdbd
Keanu Allridge <kallridge92@gmail.com>
2019-06-13 07:03:54 -0700

    Initial commit

---
+++ ContactsApi/Users/UserRegistrationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ContactsApi.Users
+{
+    public class UserRegistrationService : IUserRegistrationService
+    {
+        private UserManager<User> userManager;
+
+        public UserRegistrationService(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        // Attempt to create a new user, and return the result.
+        public async Task<IdentityResult> Register(UserRegistrationDto user)
+        {
+            // TODO: Add logger
+            var newUser = new User { Email = user.Email };
+            var result = await userManager.CreateAsync(newUser, user.Password);
+            return result;
+        }
+    }
+}
