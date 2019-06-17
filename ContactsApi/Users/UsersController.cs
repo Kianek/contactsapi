66503c4416823d59202e0b2d83c462cddaea9367
Keanu Allridge <kallridge92@gmail.com>
2019-06-17 07:47:50 -0700

    Add Delete method and refactor some code

--- ContactsApi/Users/UsersController.cs
+++ ContactsApi/Users/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ContactsApi.Users
 {
@@ -12,26 +13,35 @@ namespace ContactsApi.Users
     {
         private SignInManager<User> signInManager;
         private UserManager<User> userManager;
+        private IUserService userService;
+        private IUserRegistrationService registrationService;
 
         public UsersController(
             SignInManager<User> signInManager,
-            UserManager<User> userManager)
+            UserManager<User> userManager,
+            IUserService userService,
+            IUserRegistrationService registrationService)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+            this.userService = userService;
+            this.registrationService = registrationService;
         }
 
         // Login
-        [HttpPost]
-        public async Task<ActionResult> Login([FromBody]string email, [FromBody]string password)
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody]UserLoginDto loginDto)
         {
-            var result = await signInManager.PasswordSignInAsync(email, password, false, false);
+            // TODO: convert the user to a UserDto
+            var user = await userManager.FindByEmailAsync(loginDto.Email);
+            var result = await signInManager.PasswordSignInAsync(user, loginDto.Password, false, false);
 
             if (result.Succeeded)
             {
-                return Ok();
+                var formattedUser = userService.ConvertToDtoFromUser(user);
+                return Ok(formattedUser);
             }
-            return BadRequest("Invalid email or password");
+            return BadRequest(new { Message = "Invalid email or password" });
         }
 
         // Logout
@@ -39,19 +49,34 @@ namespace ContactsApi.Users
         public async void Logout() => await signInManager.SignOutAsync();
 
         // Register
-        [HttpPost]
-        public async Task<IActionResult> Register([FromBody]UserRegistrationDto registration)
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserRegistrationDto newUser)
         {
-            var user = new User { Email = registration.Email };
-            var result = await userManager.CreateAsync(user, registration.Password);
+            //var user = new User { UserName = registration.Username, Email = registration.Email };
+            //var result = await userManager.CreateAsync(user, registration.Password);
+            var result = await registrationService.Register(newUser);
             if (result.Succeeded)
             {
-                return Ok();
+                return Ok(new { Message = "Account created" });
             }
 
             return BadRequest(result.Errors);
         }
 
         // DeleteAccount
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody]UserRegistrationDto userToDelete)
+        {
+            var user = await userManager.FindByEmailAsync(userToDelete.Email);
+
+            if (user != null)
+            {
+                var result = await userManager.DeleteAsync(user);
+                return Ok("Account Deleted");
+            }
+
+            return BadRequest("Unable to delete account");
+        }
     }
 }
