3a7a494bc20df8546cadf538d3e04fbb14bf8b3a
Keanu Allridge <kallridge92@gmail.com>
2019-06-19 20:01:54 -0700

    Add FirstName and LastName properties

--- ContactsApi/Users/UserRegistrationDto.cs
+++ ContactsApi/Users/UserRegistrationDto.cs
@@ -7,13 +7,17 @@ namespace ContactsApi.Users
 {
     public class UserRegistrationDto
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
-        public UserRegistrationDto(string username, string email, string password)
+        public UserRegistrationDto(string userName, string firstName, string lastName, string email, string password)
         {
-            Username = username;
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
             Email = email;
             Password = password;
         }
